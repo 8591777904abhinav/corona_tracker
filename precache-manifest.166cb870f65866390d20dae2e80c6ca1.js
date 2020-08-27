77d9f1a302c7bc29f0e22c9347fe6aa48dda0801
nnhhiilliisstt <kumar.aditya221b@email.com>
2020-08-27 23:27:26 +0530

    Updates

---
+++ precache-manifest.166cb870f65866390d20dae2e80c6ca1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0915515787bdd480b3e81f6e08b2b5",
+    "url": "/corona_tracker/index.html"
+  },
+  {
+    "revision": "c166bf7b378c714e6445",
+    "url": "/corona_tracker/static/css/main.823a03b4.chunk.css"
+  },
+  {
+    "revision": "86b79fa35637187f9fac",
+    "url": "/corona_tracker/static/js/2.c45bbe4e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_tracker/static/js/2.c45bbe4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c166bf7b378c714e6445",
+    "url": "/corona_tracker/static/js/main.b2893d9f.chunk.js"
+  },
+  {
+    "revision": "f6ca6d6068e2b5a2ab61",
+    "url": "/corona_tracker/static/js/runtime-main.761f6f1b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_tracker/static/media/image.d7265326.png"
+  }
+]);
